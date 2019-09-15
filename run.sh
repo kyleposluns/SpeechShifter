e566a2def5aa2a36bcf75e6f9c9c529b80c002eb
Kyle Posluns <kyleposluns@gmail.com>
2019-09-15 11:00:01 -0400

    Yikes.

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -d "./app" ]] && [[ -f "./app/speechshifter.py" ]]; then
-  cd app || exit
-  export FLASK_APP=speechshifter.py
-  flask run
-else
-  echo "CANNOT FIND \"app\" directory"
-fi
