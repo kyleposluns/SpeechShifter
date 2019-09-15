202af3030b454dbe64e79c6cd42d5d98e12e888f
Kyle Posluns <kyleposluns@gmail.com>
2019-09-15 00:33:57 -0400

    Added a way to get transcript with a url encoded form.

--- app/download_srt.py
+++ app/download_srt.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
-from youtube_transcript_api import YouTubeTranscriptApi
-import argparse
-import regex
-from os import path
 import json
+from os import path
 
+import regex
+from youtube_transcript_api import YouTubeTranscriptApi
 
+'''
 def handle_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-y", "--youtube", help="specify a youtube video link", required=True)
     parser.add_argument("-l", "--lang", help="specify desired language", required=True)
     parser.add_argument("-o", "--output", help="specify output file", required=False, default="output.json")
     return parser.parse_args()
+'''
 
 
 def get_file_path(name, directory, extension):
@@ -29,15 +30,6 @@ def video_id(link):
     return regex.search('((?<=(v|V)/)|(?<=be/)|(?<=(\?|\&)v=)|(?<=embed/))([\w-]+)', link).group()
 
 
-args = handle_args()
-
-id = video_id(args.youtube)
-lang = args.lang
-output = args.output
-
-file = get_file_path(id, path.curdir, "json")
-
-transcripts = YouTubeTranscriptApi.get_transcript(id, languages=[lang])
-with open(file, "w") as f:
-    json.dump(transcripts, f, indent=4)
+def transcript(link, lang="en"):
+    return json.dumps(YouTubeTranscriptApi.get_transcript(video_id(link), languages=[lang]))
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from app import app
+from flask import request
+from app.download_srt import transcript
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World"
+
+
+@app.route('/transcript', methods=["GET"])
+def transcript():
+    yt_link = request.form['ytlink']
+    lang = request.form['lang']
+
+    return transcript(yt_link, lang)
