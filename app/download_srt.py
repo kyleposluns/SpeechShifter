3066ec2fb97237ab2c24ec19320c066827475901
Kyle Posluns <kyleposluns@gmail.com>
2019-09-15 00:34:50 -0400

    Removed comment in download_srt.py

--- app/download_srt.py
+++ app/download_srt.py
@@ -6,15 +6,6 @@ from os import path
 import regex
 from youtube_transcript_api import YouTubeTranscriptApi
 
-'''
-def handle_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-y", "--youtube", help="specify a youtube video link", required=True)
-    parser.add_argument("-l", "--lang", help="specify desired language", required=True)
-    parser.add_argument("-o", "--output", help="specify output file", required=False, default="output.json")
-    return parser.parse_args()
-'''
-
 
 def get_file_path(name, directory, extension):
     if not path.exists(directory + "/" + name + "." + extension):
